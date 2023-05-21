e70406374761ab3518c4a11367e9a893874bff52
ko7o2000 <ko7o1998@gmail.com>
2023-05-21 18:42:14 +0900

    Update RestaurantList.js(리뷰 숫자, 평점 넘겨주기)

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -26,23 +26,23 @@ const RestaurantList = ({setCategoryOpen, setRListOpen, setRDataOpen, menu, setM
                 <option value="RECO">추천순</option>
                 <option value="DIST">거리순</option>
             </select>
-            <RestaurantElement name='본찌' dist="0.9km" choose={onRestaurantChoose}/>
-            <RestaurantElement name='일미' dist="0.9km" choose={onRestaurantChoose}/>
-            <RestaurantElement name='옛이야기' dist="0.9km" choose={onRestaurantChoose}/>
-            <RestaurantElement name='알촌' dist="0.9km" choose={onRestaurantChoose}/>
-            <RestaurantElement name='우리집밥' dist="0.9km" choose={onRestaurantChoose}/>
-            <RestaurantElement name='포크포크' dist="0.9km" choose={onRestaurantChoose}/>
-            <RestaurantElement name='왕돈까스' dist="0.9km" choose={onRestaurantChoose}/>
-            <RestaurantElement name='오아저씨' dist="0.9km" choose={onRestaurantChoose}/>
-            <RestaurantElement name='바른김밥' dist="0.9km" choose={onRestaurantChoose}/>
+            <RestaurantElement name='본찌' dist="0.9km" choose={onRestaurantChoose} reviewnums='11' score='4.2'/>
+            <RestaurantElement name='일미' dist="0.9km" choose={onRestaurantChoose} reviewnums='166' score='4.7'/>
+            <RestaurantElement name='옛이야기' dist="0.9km" choose={onRestaurantChoose} reviewnums='22' score='4.0'/>
+            <RestaurantElement name='알촌' dist="0.9km" choose={onRestaurantChoose} reviewnums='32' score='4.4'/>
+            <RestaurantElement name='우리집밥' dist="0.9km" choose={onRestaurantChoose} reviewnums='10' score='4.0'/>
+            <RestaurantElement name='포크포크' dist="0.9km" choose={onRestaurantChoose} reviewnums='4' score='4.3'/>
+            <RestaurantElement name='왕돈까스' dist="0.9km" choose={onRestaurantChoose} reviewnums='5' score='4.6'/>
+            <RestaurantElement name='오아저씨' dist="0.9km" choose={onRestaurantChoose} reviewnums='9' score='4.1'/>
+            <RestaurantElement name='바른김밥' dist="0.9km" choose={onRestaurantChoose} reviewnums='111' score='4.0'/>
         </div>
     );
 }
 
 
-const RestaurantElement = ({name, dist, choose})=> {
+const RestaurantElement = ({name, dist, choose, reviewnums, score})=> {
     return(
-        <div onClick={()=>choose(name, '11', '4.2')} style={{border: '1px solid green'}}>
+        <div onClick={()=>choose(name, reviewnums, score)} style={{border: '1px solid green'}}>
             <p>{name}</p>
             <p>{dist}</p>
         </div>
