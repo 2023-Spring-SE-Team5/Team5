262ea1c7cc3eeb2ffe333d8d5521231d3c7d9b72
seoYoon <seoyuni0727@gmail.com>
2023-05-22 23:12:27 +0900

    .

--- src/components/RestaurantData.js
+++ src/components/RestaurantData.js
@@ -47,14 +47,12 @@ const RestaurantData = ({
 const MenuData = () => {
   return (
     <div className="w-[48%] p-[10px] border-[1px] border-solid border-green-500">
-      <MenuElement menu="메뉴" price="10000" />
-      <MenuElement menu="메뉴2" price="10000" />
-      <MenuElement menu="메뉴3" price="10000" />
-      <MenuElement menu="메뉴4" price="10000" />
-      <MenuElement menu="메뉴5" price="10000" />
-      <MenuElement menu="메뉴6" price="10000" />
-      <MenuElement menu="메뉴7" price="10000" />
-      <MenuElement menu="메뉴8" price="10000" />
+      <MenuElement menu="토핑킹&수퍼슈프림 L" price="35,400원" />
+      <MenuElement menu="립스테이크&쉬림프 L" price="36,900원" />
+      <MenuElement menu="(신제품) 토핑킹 리턴즈" price="35,900원" />
+      <MenuElement menu="디트로이트 더블 페페로니" price="32,900원" />
+      <MenuElement menu="디트로이트 미티 디럭스" price="35,900원" />
+      <MenuElement menu="(오) LA BBQ 불고기 L" price="29,900원" />
     </div>
   );
 };
