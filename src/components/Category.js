d9a725d4b1db67c614f93d5d5391e5d48c3e7f94
Suhyeon <fbtngus2789@naver.com>
2023-05-22 22:52:52 +0900

    Category 선택지 CSS 개선

--- src/components/Category.js
+++ src/components/Category.js
@@ -20,7 +20,9 @@ const Category = ({
 
   return (
     <div className="border-[1px] border-solid border-green-500 w-[80%] p-[10px] mt-[80px] mx-0">
-      <h4>메뉴를 선택하세요</h4>
+      <h4 className="w-full font-bold text-center mb-2 select-none">
+        메뉴를 선택하세요
+      </h4>
       <CategoryElement
         gotid="한식"
         imgsrc="/images/한식.jpg"
@@ -78,10 +80,16 @@ const CategoryElement = ({ gotid, imgsrc, choose }) => {
     <div
       id={gotid}
       onClick={() => choose(gotid)}
-      className="border-[1px] border-solid border-green-500 p-[10px] flex mr-0"
+      className="hover:bg-[#e1e1e1] duration-150 flex flex-col p-2 border-[1px] border-solid border-orange-500 p-[0px] flex mb-6 rounded-lg cursor-pointer"
     >
-      <img width="144px" height="96px" src={imgsrc} alt="" />
-      <p className="text-center">{gotid}</p>
+      <img
+        className="rounded-full"
+        width="144px"
+        height="96px"
+        src={imgsrc}
+        alt=""
+      />
+      <p className="text-center mt-4">{gotid}</p>
     </div>
   );
 };
